7a6d4ff5576c5ead83a93f4603c9fe7d79dce6ac
Jayden <jaydene1230@gmail.com>
2018-04-09 17:40:38 -0400

    Intial Commit, Welcome to J-RPC!

---
+++ app.js
@@ -0,0 +1,20 @@
+const { Client }  = require("discord-rpc");
+const config = require("./config.json");
+
+const rpc = new Client({ transport: "ipc" });
+
+function setRPC() {
+    global.current_game = config.rpcinfo;
+    rpc.setActivity(config.rpcinfo);
+    console.log("RPC has been set, check Discord!");
+}
+
+
+rpc.on('ready', () => {
+    console.log(`J-RPC has successfully connected to Discord ID: ${config.discordappid}.`);
+    global.current_game = {};
+    setRPC();
+});
+
+
+rpc.login(config.discordappid).catch(console.error);
